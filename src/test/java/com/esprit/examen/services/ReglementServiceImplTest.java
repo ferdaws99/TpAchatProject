71b788db2b85865a401bd13f09fa6928ed2fb6b4
MahdiKsemtini <mahdi.ksemtini@esprit.tn>
2022-11-09 16:45:48 +0100

    mahdi

---
+++ src/test/java/com/esprit/examen/services/ReglementServiceImplTest.java
@@ -0,0 +1,22 @@
+/*package com.esprit.examen.services;
+
+import static org.junit.Assert.*;
+import com.esprit.examen.entities.Reglement;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ReglementServiceImplTest {
+    @Autowired
+    IReglementService reglementService;
+
+    @Test
+    public void testAddReglement() {
+    }
+
+}
+*/
