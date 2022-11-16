788d58263a90f824640f21e589a02d3a97b24ec3
ahmedbnr <abannour@kumuluswater.com>
2022-11-16 23:19:13 +0100

    Finalized JUnit Tests For Facture Entity

--- src/test/java/com/esprit/examen/services/FactureServiceImplTest.java
+++ src/test/java/com/esprit/examen/services/FactureServiceImplTest.java
@@ -4,6 +4,7 @@ import com.esprit.examen.entities.DetailFacture;
 import com.esprit.examen.entities.Fournisseur;
 import com.esprit.examen.entities.Reglement;
 import com.esprit.examen.repositories.FactureRepository;
+import lombok.var;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Order;
@@ -41,12 +42,30 @@ public class FactureServiceImplTest {
         Assertions.assertEquals(listOperateur+1,FactureService.retrieveAllFactures().size());
     }
 
-//    @Test
-//    @Order(3)
-//    public void testUpdateFacture() {
-//        Facture p = FactureService.updateFacture(Facture.builder().montantFacture((float) 45.700).montantRemise((float) 10.200).build());
-//        Assertions.assertNotNull(p);
-//    }
+    @Test
+    @Order(3)
+    public void testCancelFacture() {
+        Facture factureAjoute = FactureService.addFacture(Facture.builder().idFacture(1L).montantFacture((float) 85.700).montantRemise((float) 25.200).dateCreationFacture(new Date()).
+                archivee(false).dateDerniereModificationFacture(new Date(0)).
+                detailsFacture((Set<DetailFacture>) new DetailFacture()).fournisseur(new Fournisseur()).reglements((Set<Reglement>) new Reglement()).build());
+        Assertions.assertNotNull(factureAjoute.getIdFacture());
+        Facture factureRecu = FactureService.retrieveFacture(factureAjoute.getIdFacture());
+        FactureService.cancelFacture(factureRecu.getIdFacture());
+        Assertions.assertTrue(factureRecu.getArchivee());
+    }
+
+    @Test
+    @Order(4)
+    public void testGetFacture() {
+        Facture factureAjoute = FactureService.addFacture(Facture.builder().idFacture(1L).montantRemise(2f).montantFacture(10f).archivee(false).build());
+        Assertions.assertNotNull(factureAjoute.getIdFacture());
+
+        Facture factureRecu = FactureService.retrieveFacture(factureAjoute.getIdFacture());
+        Assertions.assertNotNull(factureRecu.getIdFacture());
+        Assertions.assertEquals(10f, factureRecu.getMontantFacture());
+        Assertions.assertEquals(2f, factureRecu.getMontantRemise());
+
+    }
 
 
 }
